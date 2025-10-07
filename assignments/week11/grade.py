a7dd08acba78bfa7a59c7b263bf1e42e78f0d624
Wipawin Tungwattanakittikul <Wipawin.t@ku.th>
2025-10-07 14:48:41 +0700

    week11

--- assignments/week11/grade.py
+++ assignments/week11/grade.py
@@ -20,3 +20,42 @@ A global variable passing_grade = 50
 - Uses the global passing_grade to determine pass/fail
 - Prints each student's name, average, and status (PASS/FAIL)
 """
+
+#A global variable passing_grade = 50
+passing_grade = 50
+
+#(1) A function input_students(num_students)
+def input_students(num_students=0):
+    # สร้างรายชื่อนักเรียนและคะแนน 3 วิชาในรูปแบบ list ของ dictionary
+    students = [
+        {
+            'name': 'Manee',
+            'scores': [81, 72, 98]
+        },
+        {'name': 'Mana', 'scores': [70, 40, 52]}
+    ]
+    return students
+
+#(2) A function calculate_averages(students)
+def calculate_averages(students):
+    for student in students:
+        sum = 0     # ตัวแปรเก็บผลรวมคะแนน
+        for score in student['scores']:     # วนลูปคะแนนแต่ละวิชา
+            sum = sum + score       # บวกคะแนนเข้า sum
+        student['average'] = sum / len(student['scores'])       # คำนวณค่าเฉลี่ยและเพิ่มใน dictionary
+    
+    return students
+
+# (3) A function display_results(students)
+def display_results(students):
+    for student in students:    # วนลูปแต่ละคน
+        print("Name: ",student['name'])     # แสดงชื่อ
+        print("Average: %.2f" % student['average'])       # แสดงค่าเฉลี่ย
+        if student['average'] > passing_grade:      # เช็คว่าผ่านหรือไม่
+            print("Status: PASS")
+        else:
+            print("Status: FAIL")
+
+students_list = input_students()       # สร้างรายชื่อนักเรียน
+students_list = calculate_averages(students_list)       # คำนวณค่าเฉลี่ย
+display_results(students_list)      # แสดงผลลัพธ์
--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -3,7 +3,7 @@ Write a program that analyzes daily temperatures for a week:
 
 Create a function get_temperatures() that:
 
-- Uses a local list to store 7 temperature values (use input or predefined values)
+- Uses a local list to store 7 temperature values (use input or predefined values(ค่าที่กำหนดส่งล่วงหน้า))
 - Returns the list
 
 Create a function analyze_temps(temp_list) that:
@@ -22,4 +22,34 @@ Highest: 28 C
 Lowest: 19 C
 
 """
-    
+
+#"Create a function get_temperatures()"
+def get_temperatures():
+    # ใช้ list เก็บค่าอุณหภูมิ 7 วัน (ค่าที่กำหนดส่งล่วงหน้า)
+    temporatures = [32, 35, 33, 32, 29, 31, 30]
+    return temporatures
+
+#"Create a function analyze_temps(temp_list)"
+def analyze_temps(temp_list):
+    avg_temp = 0    #สร้างตัวแปรสำหรับค่าเฉลี่ย (local variable)
+    highest_temp = max(temp_list)   # หาค่าสูงสุดใน list
+    lowest_temp = min(temp_list)    # หาค่าต่ำสุดใน list
+
+    sum = 0
+    for temp in temp_list:
+        sum = sum + temp
+    avg_temp = sum/len(temp_list)   # คำนวณค่าเฉลี่ย
+    return  (avg_temp, highest_temp, lowest_temp)
+
+#"Create a function display_analysis(avg, high, low)"
+def display_analysis(avg, high, low):
+    print("Temperature Analysis for the Week:")
+    print("Average: %.2f C"%(avg))
+    print(f"Highest: {high} C")
+    print("Lowest: ", low, "C")
+    #ทำวิธีไหนก็ได้(43-45)
+
+my_temps = get_temperatures()   # เรียกใช้ฟังก์ชัน get_temperatures() เพื่อรับ list อุณหภูมิ
+analyzed_temps = analyze_temps(my_temps)    # ส่ง list ไปวิเคราะห์ หาค่าเฉลี่ย สูงสุด ต่ำสุด
+display_analysis(analyzed_temps[0], analyzed_temps[1], analyzed_temps[2])
+                #ค่าเฉลี่ย, อุณหภูมิมากสุด, อุณหภูมิต่ำสุด
